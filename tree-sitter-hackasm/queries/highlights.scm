32ad2d387eaef93f6c7d3347b6cf23f1097a696c
J. Pentz <jpentz1@gmail.com>
2024-01-09 18:46:46 -0700

    Added syntax highlighting

---
+++ tree-sitter-hackasm/queries/highlights.scm
@@ -0,0 +1,21 @@
+(builtin_regs) @parameter
+(builtin_symbols) @constant
+
+(label
+  label_name: (identifier)@label)
+
+(conditionals) @conditional
+
+"@" @character
+
+[
+ "="
+ "&"
+ "-"
+ "+"
+ "|"
+] @operator
+
+(number) @number
+
+(comment) @comment
